836666b8840ba8eb46b1a9db448f96f776360b09
osarbishei <o.sarbishei@motsai.com>
2016-06-01 11:53:23 -0400

    Re-initialize motion engine state on exit
    Targets issue #78

--- neblinaUART.py
+++ neblinaUART.py
@@ -72,8 +72,8 @@ class NeblinaUART(NeblinaAPIBase):
                 time.sleep(1)
 
         self.sc.flushInput()
-        self.stopAllStreams()
-        self.openStreaming(Interface.UART)
+        # self.stopAllStreams() # not needed since we prefer to record the motion state at the beginning before disabling everything
+        # self.openStreaming(Interface.UART) # will be done later in streammenu init function
 
     def isOpened(self, port=None):
         return self.sc and self.sc.is_open
